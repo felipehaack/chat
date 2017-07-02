6f501e9cd8a3f023ea86b924a4bb31b78fd9db01
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2017-07-02 03:22:42 -0300

    Created the rabbit mq connect provider to handle all connection and channel by manage the Future before and after (open connection -> channel -> execute block -> close channel and connection)

---
+++ chat-api/affin-api/app/com/affin/chat/service/provider/RabbitMQConnectProvider.scala
@@ -0,0 +1,39 @@
+package com.affin.chat.service.provider
+
+import javax.inject.{Inject, Singleton}
+
+import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class RabbitMQConnectProvider @Inject()(
+                                         connectionFactory: ConnectionFactory
+                                       )(
+                                         implicit private val ec: ExecutionContext
+                                       ) {
+  private def createConnection(
+                              ): Future[Connection] = Future {
+    connectionFactory.newConnection()
+  }
+
+  private def createChannel(
+                             connection: Connection
+                           ): Future[Channel] = Future {
+    connection.createChannel()
+  }
+
+  def withAsyncTransaction[A](
+                               block: Channel => Future[A]
+                             ): Future[A] = {
+
+    for {
+      connection <- createConnection()
+      channel <- createChannel(connection)
+      result <- block(channel)
+      _ = channel.close()
+      _ = connection.close()
+    } yield result
+  }
+}
