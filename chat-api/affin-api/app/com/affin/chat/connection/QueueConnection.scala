13623f8edcda03b3fb0adfaf5086b7289540a810
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2017-07-03 01:53:40 -0300

    Get a connection and channel instead of create of each call

--- chat-api/affin-api/app/com/affin/chat/connection/QueueConnection.scala
+++ chat-api/affin-api/app/com/affin/chat/connection/QueueConnection.scala
@@ -2,7 +2,7 @@ package com.affin.chat.connection
 
 import javax.inject.{Inject, Singleton}
 
-import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
+import com.rabbitmq.client.{Channel, ConnectionFactory}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
@@ -13,27 +13,15 @@ class QueueConnection @Inject()(
                                )(
                                  implicit private val ec: ExecutionContext
                                ) {
-  private def createConnection(
-                              ): Future[Connection] = Future {
-    connectionFactory.newConnection()
-  }
 
-  private def createChannel(
-                             connection: Connection
-                           ): Future[Channel] = Future {
-    connection.createChannel()
-  }
+  private val connection = connectionFactory.newConnection()
+  private val channel = connection.createChannel()
 
   def withAsyncTransaction[A](
                                block: Channel => Future[A]
                              ): Future[A] = {
-
     for {
-      connection <- createConnection()
-      channel <- createChannel(connection)
       result <- block(channel)
-      _ = channel.close()
-      _ = connection.close()
     } yield result
   }
 }
