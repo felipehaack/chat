ae05483bf172e5a85f8c02cb8c74a00bd9a45e75
Felipe Haack Schmitz <felipe.haackschmitz@gmail.com>
2017-07-04 02:09:44 -0300

    Added frontend initial structure

---
+++ chat-web/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+/*
+ import {ChatWebPage} from './app.po'
+
+ describe('Chat Web Page', () => {
+ let page: ChatWebPage;
+
+ beforeEach(() => {
+ page = new ChatWebPage();
+ });
+
+ it('should display message saying app works', () => {
+ page.navigateTo();
+ expect(page.getParagraphText()).toEqual('app works!');
+ });
+ });
+ */
---
+++ chat-web/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  URL: 'http://localhost:9000/v1'
+};
